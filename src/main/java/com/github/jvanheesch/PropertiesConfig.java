5209c49e7218c1d8dd344e656ab59eec677e6810
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-24 10:33:48 +0200

    Reverd to 'working' @PropertySource example.

--- src/main/java/com/github/jvanheesch/PropertiesConfig.java
+++ src/main/java/com/github/jvanheesch/PropertiesConfig.java
@@ -1,28 +1,21 @@
 package com.github.jvanheesch;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
-import java.io.IOException;
-import java.util.Properties;
-
-//@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.properties")
 public class PropertiesConfig {
     @Bean
-    @DependsOn("myProperties")
+//    @DependsOn("myProperties")
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-    @Bean
-    public Properties myProperties(ConfigurableEnvironment environment) throws IOException {
-        Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("application.properties"));
-        environment.getPropertySources().addFirst(new PropertiesPropertySource("test", properties));
-        return properties;
-    }
+//    @Bean
+//    public Properties myProperties(ConfigurableEnvironment environment) throws IOException {
+//        Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("application.properties"));
+//        environment.getPropertySources().addFirst(new PropertiesPropertySource("test", properties));
+//        return properties;
+//    }
 }
