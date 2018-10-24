26df3c9f4b28741f482d44c85c08098f4e67ca31
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-24 10:20:02 +0200

    Now failing.

--- src/main/java/com/github/jvanheesch/PropertiesConfig.java
+++ src/main/java/com/github/jvanheesch/PropertiesConfig.java
@@ -1,13 +1,28 @@
 package com.github.jvanheesch;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
-@PropertySource("classpath:application.properties")
+import java.io.IOException;
+import java.util.Properties;
+
+//@PropertySource("classpath:application.properties")
 public class PropertiesConfig {
     @Bean
+    @DependsOn("myProperties")
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public Properties myProperties(ConfigurableEnvironment environment) throws IOException {
+        Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("application.properties"));
+        environment.getPropertySources().addFirst(new PropertiesPropertySource("test", properties));
+        return properties;
     }
 }
