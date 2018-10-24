aa9b35f92e404a378fb1c3b51bc0b948cc936142
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-24 21:09:41 +0200

    Fixed!

--- src/test/java/com/github/jvanheesch/nested/NestedExampleTest.java
+++ src/test/java/com/github/jvanheesch/nested/NestedExampleTest.java
@@ -3,6 +3,8 @@ package com.github.jvanheesch.nested;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NestedExampleTest {
@@ -11,7 +13,16 @@ public class NestedExampleTest {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TopLevelConfig.class);
         assertThat(context.getBean("outerBean"))
                 .isNotNull();
-        assertThat(context.getBean("innerContextBean"))
-                .isNotNull();
+        assertThat(context.getBeansOfType(String.class))
+                .doesNotContainValue("innerContextBean");
+    }
+
+    @Test
+    public void testFilter() {
+        String metadataClassName = "com.github.jvanheesch.nested.ClassWithStaticContext$InnerContext";
+        Pattern pattern = Pattern.compile(".*\\$InnerContext*");
+        System.out.println(pattern.toString());
+        assertThat(pattern.matcher(metadataClassName).matches())
+                .isTrue();
     }
 }
--- src/test/java/com/github/jvanheesch/nested/TopLevelConfig.java
+++ src/test/java/com/github/jvanheesch/nested/TopLevelConfig.java
@@ -2,9 +2,17 @@ package com.github.jvanheesch.nested;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(basePackages = {"com.github.jvanheesch.nested"})
+@ComponentScan(
+        basePackages = {"com.github.jvanheesch.nested"},
+        excludeFilters = @Filter(
+                type = FilterType.REGEX,
+                pattern = ".*\\$InnerContext*"
+        )
+)
 @Configuration
 public class TopLevelConfig {
     @Bean
