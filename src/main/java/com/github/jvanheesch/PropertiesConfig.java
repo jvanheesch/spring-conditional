ecdd7eca3497a8872788a74540ae7767cd90cb4a
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-24 11:17:09 +0200

    About to test encryption with jasypt.

--- src/main/java/com/github/jvanheesch/PropertiesConfig.java
+++ src/main/java/com/github/jvanheesch/PropertiesConfig.java
@@ -7,15 +7,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @PropertySource("classpath:application.properties")
 public class PropertiesConfig {
     @Bean
-//    @DependsOn("myProperties")
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
-
-//    @Bean
-//    public Properties myProperties(ConfigurableEnvironment environment) throws IOException {
-//        Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("application.properties"));
-//        environment.getPropertySources().addFirst(new PropertiesPropertySource("test", properties));
-//        return properties;
-//    }
 }
--- src/test/java/com/github/jvanheesch/ExampleTest.java
+++ src/test/java/com/github/jvanheesch/ExampleTest.java
@@ -3,21 +3,23 @@ package com.github.jvanheesch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
 public class ExampleTest {
-    static {
-        System.setProperty("some.property", "false");
-    }
-
     @Autowired
     private SomeBean someBean;
+    @Value("${some.encrypted.property}")
+    private String someEncryptedProperty;
 
     @Test
     public void contextTest() {
-        System.out.println("ExampleTest.contextTest");
+        assertThat(someEncryptedProperty)
+                .isEqualTo("hello");
     }
 }
